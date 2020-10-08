950990ac999e5af11397fe741920e17c0e06a623
Jienie12 <jr.egmond@gmail.com>
2020-10-08 14:41:22 +0200

    Step 4  Questions visible
    + intiview / mainactivity
    + changed quetsion layout so its a bit more readable

--- MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView(){
+
+        //set layout + adapter
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+
+        //Adds questions
+        questions.add(Question(getString(R.string.valVar), false))
+        questions.add(Question(getString(R.string.ECTS), false))
+        questions.add(Question(getString(R.string.unit), true))
+        questions.add(Question(getString(R.string.whenSwitch), true))
+
     }
 }
